54ab8395f2e2f5e6b93f0735a53d1aed09f516b7
Dominic Matte <mattedominic@gmx.de>
2023-03-14 12:53:27 +0100

    addFinalFeatures

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/form/form.js";
+import List from "./components/list/list.js";
 import { uid } from "uid";
 
 function App() {
   const [activities, setActivities] = useState([]);
   const nameInputRef = useRef(null);
+  const [weather, setWeather] = useState(null);
+  const [currentWeatherIndex, setCurrentWeatherIndex] = useState(0);
+
+  const weatherAPIs = [
+    "https://example-apis.vercel.app/api/weather/sahara",
+    "https://example-apis.vercel.app/api/weather/rainforest",
+  ];
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const response = await fetch(weatherAPIs[currentWeatherIndex]);
+      const data = await response.json();
+      setWeather(data);
+    };
+
+    fetchWeather();
+
+    const intervalId = setInterval(() => {
+      setCurrentWeatherIndex(
+        (prevIndex) => (prevIndex + 1) % weatherAPIs.length
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [currentWeatherIndex]);
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(activities));
+  }, [activities]);
 
   const handleAddActivity = (activity, isGoodWeather) => {
     const newActivity = {
       id: uid(),
-      activity,
-      isGoodWeather,
+      name: activity,
+      isForGoodWeather: isGoodWeather,
     };
-    setActivities([...activities, newActivity]);
-    console.log(newActivity);
+    setActivities((prevActivities) => [...prevActivities, newActivity]);
     nameInputRef.current.focus();
   };
 
+  const handleDeleteActivity = (id) => {
+    setActivities((prevActivities) =>
+      prevActivities.filter((activity) => activity.id !== id)
+    );
+  };
+
+  const filteredActivities = activities.filter(
+    (activity) => activity.isForGoodWeather === weather?.isGoodWeather
+  );
+
   return (
     <div>
+      <h1>
+        {weather?.condition} - {weather?.temperature}&deg;C
+      </h1>
       <Form onAddActivity={handleAddActivity} nameInputRef={nameInputRef} />
-      <ul>
-        {activities.map(({ id, activity, isGoodWeather }) => (
-          <li key={id}>
-            {activity} - {isGoodWeather ? "Good Weather" : "Bad Weather"}
-          </li>
-        ))}
-      </ul>
+      <List
+        activities={filteredActivities}
+        isGoodWeather={weather?.isGoodWeather}
+        onDeleteActivity={handleDeleteActivity}
+      />
     </div>
   );
 }
